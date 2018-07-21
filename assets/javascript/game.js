191f8f0775afec6e9175cfa5796e0d6db2b7c922
Timothy Bedsole <timbedsole@gmail.com>
2018-07-21 09:48:19 -0400

    added function that assigns random values to each game button

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,6 @@
+//Object holding most important aspects of the game
 var game = {
+        //Object that holds and assigns values to elements in the game
         values: {
             wins: 0,
             losses: 0,
@@ -7,11 +9,23 @@ var game = {
             // getComputerScore :function() {
             //     return Math.floor(Math.random()*(120 - 19 + 1) + 19);
             // },
+
+            // buttonValue: Math.floor(Math.random() * 12) + 1,
             getButtonValue: function() {
                 return Math.floor(Math.random() * 12) + 1;
             }
         },
+        
+        //Object that assigns each button in HTML to a property to be targeted when the player interacts with themn
+        // elementButtons: {
+        //     earthIcon: $("#earthIcon"),
+        //     fireIcon: $("#fireIcon"),
+        //     lightningIcon: $("#lightningIcon"),
+        //     waterIcon: $("#waterIcon"),
 
+        // },
+
+        //Object that holds methods to render game's starting information
         renderScreen: {
             renderWins: function(){
                 var winsDiv = $("<div>");
@@ -27,7 +41,27 @@ var game = {
                 var computerScoreDiv = $("<div>");
                 $("#computerScore").append(computerScoreDiv);
                 computerScoreDiv.text("Target Score: " + game.values.computerScore);
+            },
+            renderPlayerScore: function(){
+                var playerScoreDiv = $("<div>");
+                $("#playerScore").append(playerScoreDiv);
+                playerScoreDiv.text(game.values.playerScore);
+            },
+            assignButtonVals: function(){
+                var initButtonValue = game.values.getButtonValue();
+                $("#earthIcon").attr("value", (initButtonValue));
+                initButtonValue = game.values.getButtonValue();
+                $("#fireIcon").attr("value", (initButtonValue));
+                initButtonValue = game.values.getButtonValue();
+                $("#lightningIcon").attr("value", (initButtonValue));
+                initButtonValue = game.values.getButtonValue();
+                $("#waterIcon").attr("value", (initButtonValue));
             }
         }
 
+
 };
+
+window.onload = function(){
+    
+}
